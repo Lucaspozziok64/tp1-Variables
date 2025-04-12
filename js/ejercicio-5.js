aa6ade0ce55fd6e8b226047e9b16976c3a5c1f2e
Lucasfi <lukafigueroa64@gmail.com>
2025-04-12 18:14:22 -0300

    programa finalizado del ejercicio-5 en js

---
+++ js/ejercicio-5.js
@@ -0,0 +1,6 @@
+// programa que pide al usuario ingresar 2 numeros con prompt y muestra de la suma
+
+let num1 = parseInt(prompt("Ingrese el primer numero"));
+let num2 = parseInt(prompt("Ingrese el segundo numero"));
+
+document.writeln(num1 + num2);
