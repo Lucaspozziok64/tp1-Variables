e2c8d38103fdd57d55cfc47659a3d9cc0ce2dae0
Lucasfi <lukafigueroa64@gmail.com>
2025-04-13 19:01:57 -0300

    programa terminado del archivo mayorEdadPS en PSeInt

--- PSeInt/mayorEdad.js
+++ PSeInt/mayorEdad.js
@@ -1,3 +1,11 @@
 // Pedir la edad y decir si es mayor de edad: Crear un algoritmo que pida la edad del usuario y 
 // determine si es mayor de edad (18 años o más).
 
+let edad = parseInt(prompt("Ingrese su edad"));
+
+if (edad >= 18) {
+    document.writeln("<h1>Es mayor de edad</h1>");
+}
+else {
+    document.writeln("<h1>Es menor de edad</h1>");
+}
