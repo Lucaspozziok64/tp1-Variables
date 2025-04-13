778aef14e1a383e81c40430e6b78f972f53c2eb9
Lucasfi <lukafigueroa64@gmail.com>
2025-04-13 18:23:11 -0300

    correcion del condicional en archivo ejercicio-11 de js

--- js/ejercicio-11.js
+++ js/ejercicio-11.js
@@ -5,10 +5,18 @@ let numero = prompt("Ingrese un número:");
 
 document.writeln(`<p>${numero} es divisible por:</p>`);
 
-if (numero % 2 === 0) document.writeln("<p>2</p>");
-if (numero % 3 === 0) document.writeln("<p>3</p>");
-if (numero % 5 === 0) document.writeln("<p>5</p>");
-if (numero % 7 === 0) document.writeln("<p>7</p>");
+if (numero % 2 === 0) {
+    document.writeln("<p>2</p>");
+}
+if (numero % 3 === 0) {
+    document.writeln("<p>3</p>");
+}
+if (numero % 5 === 0) {
+    document.writeln("<p>5</p>");
+}
+if (numero % 7 === 0) {
+    document.writeln("<p>7</p>");
+}
 
 if (!(numero % 2 === 0 || numero % 3 === 0 || numero % 5 === 0 || numero % 7 === 0)) {
     document.writeln("<p>No es divisible por 2, 3, 5 ni 7.</p>");
