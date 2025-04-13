be966637bcf5e28209a9d3ad290b967c816897dd
Lucasfi <lukafigueroa64@gmail.com>
2025-04-13 19:36:29 -0300

    agregue link js en index desde farCelcius

---
+++ PSeInt/farCelcius.js
@@ -0,0 +1,9 @@
+// Convertir grados Fahrenheit a Celsius: Crear un algoritmo que permita al usuario ingresar 
+//una temperatura en grados Fahrenheit y convertirla a grados Celsius. 
+// La fórmula para convertir de Fahrenheit a Celsius es: C= (5/9)?*(F?32)
+
+let fahrenheit = parseInt(prompt("Ingrese la temperatura en Fahrenheit"));
+
+let celsius = (5/9) * (fahrenheit-32);
+
+document.writeln(celsius);
