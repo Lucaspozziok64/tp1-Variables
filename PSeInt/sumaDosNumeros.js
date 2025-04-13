b559d8bab133c9f08a556b1444d59fe78268a296
Lucasfi <lukafigueroa64@gmail.com>
2025-04-13 19:13:43 -0300

    programa terminado del archivo sumaDosNumeros en PSeInt

---
+++ PSeInt/sumaDosNumeros.js
@@ -0,0 +1,9 @@
+//Realizar una suma de dos números: Crear un algoritmo que permita al usuario ingresar dos números,
+//realizar la suma de ambos y mostrar el resultado.
+
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+
+let suma = num1 + num2;
+
+document.writeln("La suma de los dos números es: " + suma);
