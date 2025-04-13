60009bbf1fb158f60fa81523b2e2b8dffddbb09f
Lucasfi <lukafigueroa64@gmail.com>
2025-04-13 19:19:08 -0300

    agregue link js en index desde archivo perimetroArea

--- PSeInt/perimetroArea.js
+++ PSeInt/perimetroArea.js
@@ -1,3 +1,7 @@
 // Calcular el perímetro y área de un rectángulo: Crear un algoritmo que permita al usuario ingresar
 // el largo y el ancho de un rectángulo, y calcular su perímetro y área. Las fórmulas son: Perímetro: P = 2*(largo+ancho)
 // Área: A=largo×ancho
+
+let largo = parseInt(prompt("Ingrese el largo del rectángulo"));
+
+let ancho = parseInt(prompt("Ingrese el ancho del rectángulo"));
