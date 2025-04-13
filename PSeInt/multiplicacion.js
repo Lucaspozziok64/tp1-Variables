15a1ad3c4429e48da70537b964f0e505ca807ea8
Lucasfi <lukafigueroa64@gmail.com>
2025-04-13 19:08:20 -0300

    programa terminado del archivo multiX10PS en PSeInt

--- PSeInt/multiplicacion.js
+++ PSeInt/multiplicacion.js
@@ -1,2 +1,8 @@
 // Multiplicar un número por 10: Crear un algoritmo que permita al usuario ingresar un número
 // y luego mostrar el resultado de multiplicarlo por 10.
+
+let num = parseInt(prompt("Ingrese un número"));
+
+let resultado = num * 10;
+
+document.writeln("El resultado de la multiplicación es: " + resultado);
